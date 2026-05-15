7025820b6e0b21f47b8e90f65a8cc38c46991072
Spaceonmymind <gladkih.egor@mail.ru>
2026-05-15 16:02:27 +0500

    Initial commit

--- config/config.php
+++ config/config.php
@@ -1,29 +1,35 @@
 ﻿<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 return [
-    'api_base_url' => 'https://platform-api.max.ru',
+    'api_base_url' => $_ENV['API_BASE_URL'],
 
-    'api_token' => '',
-    'bot_id' => '',
-    'bot_username' => '',
+    'api_token' => $_ENV['API_TOKEN'],
+    'bot_id' => $_ENV['BOT_ID'],
+    'bot_username' => $_ENV['BOT_USERNAME'],
 
-    'admins_users' => [
-        '***',
-    ],
-    'moderators_users' => [
-        '',
-        '',
-        '',
-        '',
-    ],
-    'moderators_chats' => [
-        '-',
-    ],
+    'admins_users' => array_filter(
+        array_map('trim', explode(',', $_ENV['ADMINS_USERS']))
+    ),
 
-    'secret_webhook' => '',
+    'moderators_users' => array_filter(
+        array_map('trim', explode(',', $_ENV['MODERATORS_USERS']))
+    ),
+
+    'moderators_chats' => array_filter(
+        array_map('trim', explode(',', $_ENV['MODERATOR_CHATS']))
+    ),
+
+    'secret_webhook' => $_ENV['SECRET_WEBHOOK'],
+
     'db' => [
-        'host' => 'localhost',
-        'user' => '***',
-        'password' => '***',
-        'dbname' => '***',
+        'host' => $_ENV['DB_HOST'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'dbname' => $_ENV['DB_NAME'],
     ],
 ];
